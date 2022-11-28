0a40a8006ac039f9fcccfca4bc2956fe80a72094
AVZhelyazkov18 <avzhelyazkov18@gmail.com>
2022-11-28 11:14:47 +0200

    + Reworked ThymeLeaf + Fixed Index.html problem with not appearing. + Log in is now connected with H2-Database

--- src/main/java/com/example/absencemanager/UserController.java
+++ src/main/java/com/example/absencemanager/UserController.java
@@ -1,18 +1,33 @@
 package com.example.absencemanager;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @GetMapping("/")
-    public String indexPage() {
+    public String Page1(@RequestParam(name = "username", required = false) String username, @RequestParam(name = "password", required = false) String password, Model model) {
+        if (username != null && password != null) {
+            Integer ans = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM USERS WHERE Username=? AND Password=?", new Object[]{username, password}, Integer.class);
+            if (ans == 1) {
+                model.addAttribute("username", username);
+                return "index";
+            } else {
+                model.addAttribute("IsNotCorrect", true);
+                return "login";
+            }
+        }
         return "index";
     }
+
 
     @GetMapping("/login")
     public String loginPage() {
@@ -22,12 +37,5 @@ public class UserController {
     @GetMapping("/registerpage")
     public String registerPage() {
         return "register";
-    }
-
-    @GetMapping(value="/log-in")
-    public String getFields(@ModelAttribute User user, Model model) {
-        model.addAttribute("loggedUser", user.getUsername());
-        System.out.println("yes");
-        return "index";
     }
 }
--- src/main/resources/static/javascript/ShowHide.js
+++
@@ -1,5 +0,0 @@
-const box = document.querySelector('.loginbutton')
-
-function removeButton() {
-    box.remove();
-}
